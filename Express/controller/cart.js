df96efd4640552fe4e2aae3f564a84680aeef8e6
wonbin <s3939906@student.rmit.edu.au>
2024-05-24 14:29:21 +0900

    0524_002
    - update addToCart data, controller, router

--- Express/controller/cart.js
+++ Express/controller/cart.js
@@ -1,8 +1,8 @@
 import * as cartRepository from '../data/cart.js';
 
-export function getCarts(req, res, next) {
-  const { id } = req.query;
-  const carts = cartRepository.getAll(id);
+export function getCarts(req, res) {
+  const { user_id } = req.params;
+  const carts = cartRepository.getAll(user_id);
   if (carts) {
     res.status(200).json(carts);
   } else {
@@ -10,9 +10,27 @@ export function getCarts(req, res, next) {
   }
 }
 
-export function updatedCartQuantity(req, res, next) {
-  const { cart_id, product_id } = req.query;
-  const { quantity } = req.body;
+export async function addToCart(req, res) {
+  const { user_id } = req.params;
+  const { product } = req.body;
+
+  // insert to DB
+  try {
+    const result = await cartRepository.addProduct(user_id, product);
+    // if result is successfully done, get cartProducts from DB for client and return it.
+    if (result.message) {
+      const carts = cartRepository.getAll(user_id);
+      res.status(201).json(carts);
+    } else {
+      res.status(404).json({ message: 'Cart product is not added' });
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+export function updatedCartQuantity(req, res) {
+  const { cart_id, product_id, quantity } = req.body;
 
   const carts = cartRepository.update(cart_id, product_id, quantity);
   if (carts) {
@@ -22,8 +40,8 @@ export function updatedCartQuantity(req, res, next) {
   }
 }
 
-export function deleteCart(req, res, next) {
-  const { cart_id, product_id } = req.query;
+export function deleteCart(req, res) {
+  const { cart_id, product_id } = req.body;
   cartRepository.remove(cart_id, product_id);
   res.status(204);
 }
