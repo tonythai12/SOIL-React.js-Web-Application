ee3ae8bd3f70fe2abbc006337189592e21c3f1b2
wonbin <s3939906@student.rmit.edu.au>
2024-05-26 20:44:27 +0900

    0526_002
    - update special sale products

--- Express/data/specialsale.js
+++ Express/data/specialsale.js
@@ -1,5 +1,52 @@
 import db from '../db/database.js';
 
+export async function getAll() {
+  // get SpecialSale
+  const [saleProducts] = db.execute('SELECT * FROM SpecialSale');
+
+  if (saleProducts.length > 0) {
+    // get products
+    const productIds = saleProducts.map((res) => res.product.id);
+    const products = productIds.map((ids) => {
+      const res = db.execute(
+        'SELECT * FROM Products WHERE product_id=? VALUES (?)',
+        [ids]
+      );
+      return res[0];
+    });
+
+    // const resultArr = saleProducts[0].map((sale) => {
+    //   const specialArr = products.filter((product) =>
+    //     sale.product_name.includes(product.name)
+    //   );
+
+    //   return {
+    //     [sale.product_name]: {
+    //       tips: sale.tip,
+    //       specials: specialArr,
+    //       imageUrl: sale.imageUrl,
+    //     },
+    //   };
+    // });
+
+    const resultObject = saleProducts[0].reduce((acc, sale) => {
+      const specialArr = products.filter((product) =>
+        sale.product_name.includes(product.name)
+      );
+
+      acc[sale.product_name] = {
+        tips: sale.tip,
+        specials: specialArr,
+        imageUrl: sale.imageUrl,
+      };
+
+      return acc;
+    }, {});
+
+    return resultObject;
+  }
+}
+
 export async function get(user_id) {
   return db
     .execute('SELECT * FROM Preference WHERE user_id=? VALUES (?)', [user_id])
