e9b17593ceb8f9dc3e3e8e96a6c27f2b7f893352
wonbin <s3939906@student.rmit.edu.au>
2024-05-28 17:47:40 +0900

    0528_013
    -http.js :  add options.body when request clearly

--- React/src/network/http.js
+++ React/src/network/http.js
@@ -9,33 +9,37 @@ export default class HttpClient {
 
   async fetch(url, options = {}) {
     try {
-      const headers = options.body instanceof FormData 
-        ? {} 
-        : { 'Content-Type': 'application/json' }; 
-      
+      const headers =
+        options.body instanceof FormData
+          ? {}
+          : { 'Content-Type': 'application/json' };
+
       const response = await this.client.request({
         url,
         ...options,
-        headers
+        headers,
+        data: options.body,
       });
-      
+
       return {
         status: response.status,
-        data: response.data
+        data: response.data,
       };
     } catch (error) {
       const status = error.response ? error.response.status : 500;
-      const message = error.response && error.response.data && error.response.data.message 
-        ? error.response.data.message
-        : 'Something went wrong! 🤪';
-      const type = error.response && error.response.data && error.response.data.type
-        ? error.response.data.type 
-        : null;
-        
+      const message =
+        error.response && error.response.data && error.response.data.message
+          ? error.response.data.message
+          : 'Something went wrong! 🤪';
+      const type =
+        error.response && error.response.data && error.response.data.type
+          ? error.response.data.type
+          : null;
+
       return {
         status,
         message,
-        type
+        type,
       };
     }
   }
