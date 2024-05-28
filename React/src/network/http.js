e2c27fc6e1214dc1b4c1280fff9a05a37dfd516a
wonbin2da <144112484+wonbin2da@users.noreply.github.com>
2024-05-28 17:09:49 +1000

    Update http.js

--- React/src/network/http.js
+++ React/src/network/http.js
@@ -4,32 +4,39 @@ export default class HttpClient {
   constructor(baseURL) {
     this.client = axios.create({
       baseURL: baseURL,
-      headers: {
-        'Content-Type': 'application/json',
-      },
     });
   }
 
   async fetch(url, options = {}) {
     try {
+      const headers = options.body instanceof FormData 
+        ? {} // FormData일 경우 빈 객체
+        : { 'Content-Type': 'application/json' }; // 그 외엔 json
+      
       const response = await this.client.request({
         url,
         ...options,
+        headers
       });
-
-      return { status: response.status, data: response.data };
+      
+      return {
+        status: response.status,
+        data: response.data
+      };
     } catch (error) {
       const status = error.response ? error.response.status : 500;
-      const message =
-        error.response && error.response.data && error.response.data.message
-          ? error.response.data.message
-          : 'Something went wrong! 🤪';
-      const type =
-        error.response && error.response.data && error.response.data.type
-          ? error.response.data.type
-          : null;
-
-      return { status, message, type };
+      const message = error.response && error.response.data && error.response.data.message 
+        ? error.response.data.message
+        : 'Something went wrong! 🤪';
+      const type = error.response && error.response.data && error.response.data.type
+        ? error.response.data.type 
+        : null;
+        
+      return {
+        status,
+        message,
+        type
+      };
     }
   }
 }
